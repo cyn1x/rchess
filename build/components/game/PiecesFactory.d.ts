401d0f87884515aeb889e36e96ceaf49be889a9a
Zac Vuković <zmadden@myune.edu.au>
2020-01-27 14:21:13 +1100

    Remove old build files that were present after the last major refactor

--- build/components/game/PiecesFactory.d.ts
+++
@@ -1,6 +0,0 @@
-import { IPiece } from './pieces/types';
-import Pieces from './pieces/Pieces';
-declare class PiecesFactory extends Pieces {
-    typeOfPiece(piece: string): IPiece;
-}
-export default PiecesFactory;
