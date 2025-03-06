962a01aaa9c89e62aa505c5d5f596e997a814619
Zac Vukovic <26176372+cyn1x@users.noreply.github.com>
2025-03-07 01:13:45 +1100

    Add websocket-rs as dependency for web client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::thread;
+use websocket_rs as websocket;
+
 fn main() {
-    println!("Hello, world!");
+    // let event = websocket::init();
+
+    thread::spawn(|| {
+        websocket::serve();
+    });
+
+    let thread_join_handle = thread::spawn(move || {
+        loop {
+            // let recv = event.recv_msg();
+
+            // let send = ;
+            // event.send_msg(send.to_string());
+        }
+    });
+
+    let _res = thread_join_handle.join();
 }
